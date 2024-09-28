1f7aa1eff0fcd1b465ce43d3d88850a3dffaee33
bwestwood11 <dabrettwestwood@gmail.com>
2024-09-28 12:14:30 -0400

    hint docs updated

--- app/docs/[slug]/page.tsx
+++ app/docs/[slug]/page.tsx
@@ -3,6 +3,8 @@ import { documents } from "#site/content";
 import { notFound } from "next/navigation";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { DashboardTableOfContents } from "@/components/toc";
+import { ChevronRightIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 function getDocumentBySlug(slug: string) {
   return documents.find((document) => document.slug === slug);
@@ -13,8 +15,26 @@ export default function Home({ params }: { params: { slug: string } }) {
   if (!doc) return notFound();
   return (
     <div className="flex gap-3">
-      <div className="w-full py-6 lg:py-8 px-3">
+       <div className="mx-auto flex flex-col w-full min-w-0 py-6 lg:py-8 px-3">
+        <div className="mb-4 flex items-center space-x-1 text-sm leading-none text-muted-foreground">
+          <div className="truncate">Docs</div>
+          <ChevronRightIcon className="h-3.5 w-3.5" />
+          <div className="text-foreground">{doc.title}</div>
+        </div>
+        <div className="space-y-2">
+          <h1 className={cn("scroll-m-20 text-3xl font-bold tracking-tight")}>
+            {doc.title}
+          </h1>
+          {doc.description && (
+            <p className="text-base text-muted-foreground">
+              <p>{doc.description}</p>
+            </p>
+          )}
+        </div>
+      
+      <div className="w-full mt-6">
         <MDXContent code={doc.content} />
+      </div>
       </div>
       <div className="hidden text-sm xl:block py-6 lg:py-8">
         <div className="sticky top-20 h-[calc(100vh-3.5rem)]">
